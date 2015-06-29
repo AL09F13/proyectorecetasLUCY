ca022c7514343ed091954eb80847dabcc5f820aa
AL09F13 <c4rt3r97@gmail.com>
2015-06-28 19:26:36 -0500

    UNO
    RECETAS

--- js/acciones.js
+++ js/acciones.js
@@ -3,30 +3,21 @@ $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	
 	
-	$ ('#oaxaca') .on ('tap' , function () { 
+	$ ('#España') .on ('tap' , function () { 
   navigator .vibrate (1000);
  });//  tap a btnvibrar
 
-$ ('#guelaguetza') .on ('tap' , function () { 
+$ ('#Veracruz') .on ('tap' , function () { 
   navigator .vibrate (1000);
  });//  tap a btnvibrar
 
-$ ('#tule') .on ('tap' , function () { 
+$ ('#Mexico') .on ('tap' , function () { 
   navigator .vibrate (1000);
  });//  tap a btnvibrar
 
-$ ('#soledad') .on ('tap' , function () { 
+$ ('#Puebla') .on ('tap' , function () { 
   navigator .vibrate (1000);
  });//  tap a btnvibrar
-
-$ ('#monte') .on ('tap' , function () { 
-  navigator .vibrate (1000);
- });//  tap a btnvibrar
-
-$ ('#domingo') .on ('tap' , function () { 
-  navigator .vibrate (1000);
- });//  tap a btnvibrar
-
 }); 
 });
 
